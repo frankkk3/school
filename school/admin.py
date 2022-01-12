4fc727285cc780d0f40283e279aa5a38946f90e7
frankkk3 <97428913+frankkk3@users.noreply.github.com>
2022-01-12 20:13:31 +0700

    Add files via upload

---
+++ school/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+
+admin.site.register(Profile)
+
+admin.site.register(proshow)
+admin.site.register(ALLteacher)
+admin.site.register(Category)
+admin.site.register(yearr)
+admin.site.register(levell)
+admin.site.register(Comment)
