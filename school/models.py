63b16607e940e988e203c73cc9a6820fbac7636b
frankkk3 <97428913+frankkk3@users.noreply.github.com>
2022-01-12 20:44:25 +0700

    Add files via upload

--- school/models.py
+++ school/models.py
@@ -1,7 +1,7 @@
 
 from django.db import models
 from django.db.models.expressions import F
-from years.models import levelschool, yearsschool
+
 from PIL import Image
 from django.urls import reverse
 # Create your models here.
